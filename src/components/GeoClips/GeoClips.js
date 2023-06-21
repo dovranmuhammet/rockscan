4e4642f5a4c068bc708799a6f320783cefa6b678
Dovran <dovranrustemov@gmail.com>
2023-06-20 20:58:58 -0400

    added card for geoinfo

--- src/components/GeoClips/GeoClips.js
+++ src/components/GeoClips/GeoClips.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
 import geoInfo from './geoinfo.json'
 import Geocss from './GeoClips.css'
 
@@ -7,13 +13,22 @@ function GeoClips() {
   const [data, useData] = useState([geoInfo])
 
   console.log(data.map((item) => item.topic))
+
   return (
-    <div className='geoInfo-card'>
-      <div className='topic'>{data.map((item) => item[0].topic)}</div>
-      <div className='description'>
-        {data.map((item) => item[0].description)}
-      </div>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          {data.map((item) => item[0].topic)}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          {data.map((item) => item[0].description)}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Share</Button>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
+    </Card>
   )
 }
 export default GeoClips

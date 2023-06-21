d24037b4ab892c3bb4023ae963f6a7493b33b4a4
Dovran <dovranrustemov@gmail.com>
2023-06-21 14:28:22 -0400

    Working on remark button

--- src/components/GeoExplore/GeoClips.js
+++ src/components/GeoExplore/GeoClips.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'

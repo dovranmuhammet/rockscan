54e1063f806e49622d7db3165bb5155b9c8deadc
Dovran <dovranrustemov@gmail.com>
2023-06-21 21:58:57 -0400

    Added mark button to the card

--- src/components/GeoExplore/GeoClips.js
+++ src/components/GeoExplore/GeoClips.js
@@ -4,15 +4,34 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import geoInfo from './geoinfo.json'
-import Geocss from './GeoClips.css'
+import geoinfo from './geoinfo.json'
+import Geoclips from './Geoclips.css'
 
 function GeoClips() {
+  const [geoInfo, setGeoInfo] = useState(geoinfo)
+
+  const handleMarkClick = (itemId) => {
+    const updatedGeoInfo = geoInfo.map((item) => {
+      if (item.id === itemId) {
+        return {
+          ...item,
+          isMarked: !item.isMarked,
+        }
+      }
+      return item
+    })
+    setGeoInfo(updatedGeoInfo)
+  }
+
   return (
     <div className='card-container'>
       {geoInfo.map((item) => (
         <Card key={item.id} className='card'>
           <CardContent>
+            <div
+              className={`mark ${item.isMarked ? 'clicked' : ''}`}
+              onClick={() => handleMarkClick(item.id)}
+            ></div>
             <div className='image-container'>
               <img src={item.img} alt='Earth' className='card-image' />
             </div>

fc1363651252ced6104b002ab1460ae835e1d199
Dovran <dovranrustemov@gmail.com>
2023-06-22 00:02:31 -0400

    Changed component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/header/Navbar'
 import Footer from './components/Footer/Footer'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import GeoClips from './components/GeoExplore/GeoClips'
+import GeoClips from './components/GeoClips/GeoClips'
 
 function App() {
   return (

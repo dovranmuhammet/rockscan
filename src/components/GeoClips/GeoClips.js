8329cee41cdab17ade506d075ff96ec908296c6d
Dovran <dovranrustemov@gmail.com>
2023-06-22 20:13:07 -0400

    Added data for geoclips comp

--- src/components/GeoClips/GeoClips.js
+++ src/components/GeoClips/GeoClips.js
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import geoinfo from './geoinfo.json'
-import Geoclips from './GeoClips.css'
+import Geodata from './GeoClips.css'
 
 function GeoClips() {
   const [geoInfo, setGeoInfo] = useState(geoinfo)

60263d7a6e91c759291d1da2369867ebdd747297
Dovran <dovranrustemov@gmail.com>
2023-06-23 00:56:54 -0400

    Added css file

--- src/components/GeoClips/GeoClips.js
+++ src/components/GeoClips/GeoClips.js
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import geoinfo from './geoinfo.json'
-import styles from './geoclips.css'
+import clipcss from '.../GeoClips/geoclips.css'
 
 // Access the CSS class names as properties of the imported 'styles' object
 // Example: <div className={styles.container}></div>

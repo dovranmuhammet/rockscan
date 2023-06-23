68ee5481019588ea6ed1610e6bc7bb7df0d23eac
Dovran <dovranrustemov@gmail.com>
2023-06-23 01:40:59 -0400

    Added in appcss

--- src/components/GeoClips/GeoClips.js
+++ src/components/GeoClips/GeoClips.js
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import geoinfo from './geoinfo.json'
-import './GeoClips.css'
 
 function GeoClips() {
   const [geoInfo, setGeoInfo] = useState(geoinfo)

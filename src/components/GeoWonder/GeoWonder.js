ccc2aa96b1f269244074f8f3f669da32d4efb542
Dovran <dovranrustemov@gmail.com>
2023-07-20 13:42:07 -0400

    Added select option topic

--- src/components/GeoWonder/GeoWonder.js
+++ src/components/GeoWonder/GeoWonder.js
@@ -10,10 +10,12 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import './GeoWonder.css'
 import geoinfo from './geoinfo.json'
 import { FaWhatsapp, FaInstagram, FaTwitter, FaEnvelope } from 'react-icons/fa'
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 
 function GeoWonder() {
   const [geoInfo, setGeoInfo] = useState(geoinfo)
   const [sharePopup, setSharePopup] = useState(null)
+  const [selectedTopic, setSelectedTopic] = useState(null)
 
   const handleHeartClick = (itemId) => {
     setGeoInfo((prevGeoInfo) =>
@@ -51,6 +53,11 @@ function GeoWonder() {
 
   const websiteLink = 'https://www.rockscanner.com'
 
+  const handleTopicSelect = (event) => {
+    const selectedValue = event.target.value
+    setSelectedTopic(selectedValue)
+  }
+
   return (
     <div className='card-container'>
       <button
@@ -78,200 +85,224 @@ function GeoWonder() {
         <ArrowUpwardIcon style={{ fontSize: '24px' }} />
       </button>
 
-      {geoInfo.map((item) => (
-        <Card
-          key={item.id}
-          className='card'
-          data-description={item.description}
+      <FormControl variant='outlined' className='topic-select-container'>
+        <InputLabel>Topics</InputLabel>
+        <Select
+          native
+          value={selectedTopic}
+          onChange={handleTopicSelect}
+          label='Topics'
+          className='topic-select'
         >
-          <CardContent>
-            <div className='image-container'>
-              <img src={item.img} alt='Earth' className='card-image' />
-            </div>
-
-            <Typography
-              gutterBottom
-              variant='h5'
-              component='div'
-              className='card-title'
-              style={{
-                color: 'rgb(34, 68, 123)',
-                fontWeight: '550',
-                marginTop: '15px',
-              }}
-            >
+          <option value='selectedTopic'>Select a topic</option>
+          {geoInfo.map((item) => (
+            <option key={item.id} value={item.topic}>
               {item.topic}
-            </Typography>
+            </option>
+          ))}
+        </Select>
+      </FormControl>
 
-            <Typography
-              variant='body2'
-              color='text.secondary'
-              className='card-description-wonder'
-              style={{
-                marginTop: '15px',
-                lineHeight: '25px',
-                color: '#333',
-                fontSize: '14px',
-              }}
-            >
-              {item.showFullDescription
-                ? `${item.description}`
-                : `${item.description.split('. ').slice(0, 3).join('. ')}.`}
-            </Typography>
+      {geoInfo
+        .filter((item) => !selectedTopic || item.topic === selectedTopic)
+        .map((item) => (
+          <Card
+            key={item.id}
+            className='card'
+            data-description={item.description}
+          >
+            <CardContent>
+              <div className='image-container'>
+                <img src={item.img} alt='Earth' className='card-image' />
+              </div>
 
-            {item.description.split('. ').length > 1 && (
-              <Button
-                onClick={() => toggleDescription(item.id)}
+              <Typography
+                gutterBottom
+                variant='h5'
+                component='div'
+                className='card-title'
                 style={{
-                  fontWeight: 'bold',
-                  marginTop: '10px',
-                  color: '#345a8b',
-                  cursor: 'pointer',
+                  color: 'rgb(34, 68, 123)',
+                  fontWeight: '550',
+                  marginTop: '15px',
                 }}
               >
-                {item.showFullDescription ? 'Read Less' : 'Read More'}
-              </Button>
-            )}
+                {item.topic}
+              </Typography>
 
-            {/* Add the animated fun_fact section */}
-            <div className='card-fun-fact'>
-              <span className='fun-fact-icon' role='img' aria-label='Fun Fact'>
-                🎉
-              </span>
-              Fun Fact: {item.fun_fact}
-            </div>
+              <Typography
+                variant='body2'
+                color='text.secondary'
+                className='card-description-wonder'
+                style={{
+                  marginTop: '15px',
+                  lineHeight: '25px',
+                  color: '#333',
+                  fontSize: '14px',
+                }}
+              >
+                {item.showFullDescription
+                  ? `${item.description}`
+                  : `${item.description.split('. ').slice(0, 3).join('. ')}.`}
+              </Typography>
 
-            {/* Add the external links */}
-            {item.external_links && item.external_links.length > 0 && (
-              <div className='external-links'>
-                <Typography
-                  variant='body2'
-                  color='text.secondary'
+              {item.description.split('. ').length > 1 && (
+                <Button
+                  onClick={() => toggleDescription(item.id)}
                   style={{
                     fontWeight: 'bold',
-                    marginBottom: '8px',
+                    marginTop: '10px',
                     color: '#345a8b',
-                    marginTop: '15px',
+                    cursor: 'pointer',
                   }}
                 >
-                  External Links:
-                </Typography>
-                {item.external_links.map((link) => (
-                  <Button
-                    key={link.id}
-                    variant='outlined'
-                    startIcon={<OpenInNewIcon />}
-                    href={link.url}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='external-link-button'
+                  {item.showFullDescription ? 'Read Less' : 'Read More'}
+                </Button>
+              )}
+
+              {/* Add the animated fun_fact section */}
+              <div className='card-fun-fact'>
+                <span
+                  className='fun-fact-icon'
+                  role='img'
+                  aria-label='Fun Fact'
+                >
+                  🎉
+                </span>
+                Fun Fact: {item.fun_fact}
+              </div>
+
+              {/* Add the external links */}
+              {item.external_links && item.external_links.length > 0 && (
+                <div className='external-links'>
+                  <Typography
+                    variant='body2'
+                    color='text.secondary'
                     style={{
+                      fontWeight: 'bold',
+                      marginBottom: '8px',
                       color: '#345a8b',
-                      borderColor: '#345a8b',
-                      marginRight: '10px',
+                      marginTop: '15px',
                     }}
                   >
-                    {link.title}
-                  </Button>
-                ))}
-              </div>
-            )}
-          </CardContent>
-          <CardActions className='card-actions'>
-            <Button
-              className={`like-button ${item.isMarked ? 'liked' : ''}`}
-              size='small'
-              onClick={(event) => handleShareClick(event, item.id)}
-              style={{
-                backgroundColor: '#345a8b',
-                color: '#fff',
-                borderRadius: '6px',
-                padding: '8px 16px',
-                fontWeight: 'bold',
-                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-                transition:
-                  'background-color 0.3s, transform 0.3s, box-shadow 0.3s',
-                cursor: 'pointer',
-                textTransform: 'uppercase',
-              }}
-            >
-              Share
-            </Button>
-
-            <div className='App'>
-              <Heart
-                isClick={item.isClick}
-                onClick={() => handleHeartClick(item.id)}
-              />
-            </div>
-          </CardActions>
-
-          {sharePopup === item.id && (
-            <div className='share-popup'>
-              <div className='share-popup-icons'>
-                <a
-                  href={`mailto:?subject=${encodeURIComponent(
-                    item.topic
-                  )}&body=${encodeURIComponent(
-                    `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}\n\n`
-                  )}`}
-                  className='share-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FaEnvelope size={32} />
-                </a>
-
-                <a
-                  href={`whatsapp://send?text=${encodeURIComponent(
-                    `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}\n\n`
-                  )}`}
-                  className='share-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FaWhatsapp size={32} />
-                </a>
-
-                <a
-                  href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
-                    `${item.link}`
-                  )}&text=${encodeURIComponent(
-                    `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}`
-                  )}`}
-                  className='share-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FaTwitter size={32} />
-                </a>
-
-                <a
-                  href={`https://www.instagram.com/share?url=${encodeURIComponent(
-                    `${item.link}`
-                  )}&caption=${encodeURIComponent(
-                    `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}`
-                  )}`}
-                  className='share-button'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <FaInstagram size={32} />
-                </a>
-              </div>
+                    External Links:
+                  </Typography>
+                  {item.external_links.map((link) => (
+                    <Button
+                      key={link.id}
+                      variant='outlined'
+                      startIcon={<OpenInNewIcon />}
+                      href={link.url}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      className='external-link-button'
+                      style={{
+                        color: '#345a8b',
+                        borderColor: '#345a8b',
+                        marginRight: '10px',
+                      }}
+                    >
+                      {link.title}
+                    </Button>
+                  ))}
+                </div>
+              )}
+            </CardContent>
+            <CardActions className='card-actions'>
               <Button
-                className='close-button'
-                onClick={handleClosePopup}
+                className={`like-button ${item.isMarked ? 'liked' : ''}`}
+                size='small'
+                onClick={(event) => handleShareClick(event, item.id)}
                 style={{
+                  backgroundColor: '#345a8b',
+                  color: '#fff',
+                  borderRadius: '6px',
+                  padding: '8px 16px',
                   fontWeight: 'bold',
+                  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+                  transition:
+                    'background-color 0.3s, transform 0.3s, box-shadow 0.3s',
+                  cursor: 'pointer',
+                  textTransform: 'uppercase',
                 }}
               >
-                Close
+                Share
               </Button>
-            </div>
-          )}
-        </Card>
-      ))}
+
+              <div className='App'>
+                <Heart
+                  isClick={item.isClick}
+                  onClick={() => handleHeartClick(item.id)}
+                />
+              </div>
+            </CardActions>
+
+            {sharePopup === item.id && (
+              <div className='share-popup'>
+                <div className='share-popup-icons'>
+                  <a
+                    href={`mailto:?subject=${encodeURIComponent(
+                      item.topic
+                    )}&body=${encodeURIComponent(
+                      `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}\n\n`
+                    )}`}
+                    className='share-button'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <FaEnvelope size={32} />
+                  </a>
+
+                  <a
+                    href={`whatsapp://send?text=${encodeURIComponent(
+                      `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}\n\n`
+                    )}`}
+                    className='share-button'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <FaWhatsapp size={32} />
+                  </a>
+
+                  <a
+                    href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                      `${item.link}`
+                    )}&text=${encodeURIComponent(
+                      `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}`
+                    )}`}
+                    className='share-button'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <FaTwitter size={32} />
+                  </a>
+
+                  <a
+                    href={`https://www.instagram.com/share?url=${encodeURIComponent(
+                      `${item.link}`
+                    )}&caption=${encodeURIComponent(
+                      `${item.topic}\n\n${item.description}\n\nVisit my website at ${websiteLink}`
+                    )}`}
+                    className='share-button'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <FaInstagram size={32} />
+                  </a>
+                </div>
+                <Button
+                  className='close-button'
+                  onClick={handleClosePopup}
+                  style={{
+                    fontWeight: 'bold',
+                  }}
+                >
+                  Close
+                </Button>
+              </div>
+            )}
+          </Card>
+        ))}
     </div>
   )
 }
